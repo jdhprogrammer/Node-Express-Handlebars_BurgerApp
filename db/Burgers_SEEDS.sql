e098b025dd5e6a27a0ce34fd3b29e77a7aca6fdf
David Harris <jdh.programmer@gmail.com>
2020-12-22 13:35:01 -0600

    add start script in package.json

--- db/Burgers_SEEDS.sql
+++ db/Burgers_SEEDS.sql
@@ -2,11 +2,17 @@
 -- Query: SELECT * FROM burgers_db.burgers
 LIMIT 0, 50
 
--- Date: 2020-12-21 20:55
+-- Date: 2020-12-22 13:21
 */
 INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (2,'Impossible Veggie Burger',0);
 INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (3,'Plano Burger',0);
-INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (4,'Choco Burger with Chocolate Ganache',0);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (4,'Choco Burger with Chocolate Ganache',1);
 INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (5,'Not Yo Burger',0);
 INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (6,'Italian Herbs and Cheese Burger',0);
 INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (7,'Blue Cheese Burger w/ Grilled Onions & Grilled Jalapenos',0);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (12,'Million Island Tuxedo Burger',0);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (14,'Sloppy Sloppy Bacon Cheese Burger',1);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (15,'The Thickness',1);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (17,'Hunger Game Burger',1);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (18,'I Aint No Jala-Peno Burger',1);
+INSERT INTO `burgers` (`id`,`burger_name`,`scarfed`) VALUES (19,'VolunDeer as Tribute Burger',1);
