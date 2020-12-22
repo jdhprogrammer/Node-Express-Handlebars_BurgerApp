68f9b43263fd71721662cc59034d01d038e62fcd
David Harris <jdh.programmer@gmail.com>
2020-12-22 13:41:15 -0600

    added process.env.jaws if statement to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+
+
+if (process.env.JAWSDB_URL) {
+    // eslint-disable-next-line no-use-before-define
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    // eslint-disable-next-line no-var
+
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
 connection.connect(function(err) {
